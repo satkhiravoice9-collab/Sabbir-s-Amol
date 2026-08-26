8072b5384a7d0b66957c8680f939c66512321994
satkhiravoice9-collab <satkhiravoice9@gmail.com>
2026-08-26 23:23:39 +0600

    Refactor package name and improve comment clarity
    Updated package name and cleaned up comments in MainActivity.kt.

--- app/src/main/java/com/sabbirsamol/app/MainActivity.kt
+++ app/src/main/java/com/sabbirsamol/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sabbir.amol // আপনার প্রজেক্টের প্যাকেজ নাম অনুযায়ী প্রয়োজন হলে পরিবর্তন করুন
+package com.sabbir.amol
 
 import android.annotation.SuppressLint
 import android.os.Bundle
@@ -10,34 +10,27 @@ import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var webView: WebView
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // WebView তৈরি ও সেটআপ
         webView = WebView(this)
         setContentView(webView)
 
         val settings: WebSettings = webView.settings
-        settings.javaScriptEnabled = true                  // জাভাস্ক্রিপ্ট সক্রিয় করা
-        settings.domStorageEnabled = true                  // কাজা ট্র্যাকার ও ডার্ক মোডের লোকাল স্টোরেজ সাপোর্ট
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
         settings.databaseEnabled = true
         settings.allowFileAccess = true
         settings.allowContentAccess = true
         settings.loadWithOverviewMode = true
         settings.useWideViewPort = true
-        settings.cacheMode = WebSettings.LOAD_DEFAULT
 
         webView.webViewClient = WebViewClient()
         webView.webChromeClient = WebChromeClient()
-
-        // assets ফোল্ডার থেকে আপনার মূল index.html লোড করা
         webView.loadUrl("file:///android_asset/index.html")
 
-        // ব্যাক বাটন চাপলে অ্যাপ থেকে বের না হয়ে পেইজের ভেতরে ব্যাকে যাওয়ার হ্যান্ডলার
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 if (webView.canGoBack()) {
