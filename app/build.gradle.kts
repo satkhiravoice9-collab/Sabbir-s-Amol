c113fb7cc7f414c90396497afc3f9512da25d8c5
satkhiravoice9-collab <satkhiravoice9@gmail.com>
2026-08-27 10:01:51 +0600

    Update applicationId and namespace in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.satkhiravoice.sabbirsamol"
+    namespace = "com.sabbir.amol"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.satkhiravoice.sabbirsamol"
+        applicationId = "com.sabbir.amol"
         minSdk = 23
         targetSdk = 34
         versionCode = 1
